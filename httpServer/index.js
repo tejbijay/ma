7541f4c18bcbc67d30ed75b42e47a76d03c9137e
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-19 09:19:18 +0545

    third commit

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,14 +1,27 @@
-// const http = require('http');
 
+
+// const http = require('http');
+// const chalk = require('chalk');
 
 // const server = http.createServer((req,res)=>{
-    
-//     res.end("hello, my name is Tej Singh Chaudhary");
+//     if(req.url == "/"){
+//         res.end("this is home page");
+//     }
+//     else if 
+//     (req.url == "/about"){
+//         res.end("this is about page");
+//     }
+//    else{
+//        res.statusCode(404);
+//     //    res.end("page doesnt exist");
+//    }
 // });
 
 // server.listen(8080,'127.0.0.1',()=>{
-//     console.log("server created");
+//     console.log(chalk.green("server has been created"));
 // });
+
+
 
 
 
@@ -19,11 +32,20 @@ const http = require('http');
 const chalk = require('chalk');
 
 const server = http.createServer((req,res)=>{
-    res.end("hello");
+    if(req.url == '/'){
+        res.end("hello, my name is tej singh chuahdary");
+    }
+     else if (req.url == '/student'){
+
+                res.end('this is student page');
+
+    }
+    else
+    res.end('404 error');
 });
 
 server.listen(8080,'127.0.0.1',()=>{
-    console.log(chalk.green("server has been created"));
+    console.log(chalk.red.bgGreen("server has been created"));
 });
 
 
