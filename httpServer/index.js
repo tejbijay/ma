7644e259a060c24397cf593aabcbd3aa1c84df9b
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-25 15:32:11 +0545

    simple api built

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,6 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
+
+   const data =  fs.readFileSync(`${__dirname}/userApi/userapi.json`,"utf-8");
+
+    const objData = JSON.parse(data);
+
     if(req.url =='/'){
         res.end('this is home page');
     }
@@ -11,8 +17,12 @@ const server = http.createServer((req,res)=>{
     else if (req.url == '/singh'){
         res.end('this is singh page');
     }
+    else if (req.url == '/userapi'){
+        res.end(objData[2].name);
+        // res.end('hello from user api page');
+    }
     else
-    {
+    { 
         res.writeHead(404, { "content-type":"text/html"});
         res.end('page not found');
     }
