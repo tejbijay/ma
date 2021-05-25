0ac99da61cbad6406b456f2ad6d8dcc821a22cb4
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-25 16:39:38 +0545

    code status print through events

--- EventMod/index.js
+++ EventMod/index.js
@@ -1,12 +1,45 @@
+// const EventEmitter = require('events');
+
+// const event = new EventEmitter();
+
+
+// event.on("my name is",()=>{
+//     console.log("Tej");
+// });
+
+
+
+// event.emit("my name is");
+
+
+
 const EventEmitter = require('events');
+const { emit } = require('process');
+
 
 const event = new EventEmitter();
 
 
-event.on("my name is",()=>{
-    console.log("Tej");
+// event.on("my name is",()=>{
+//     console.log("Tej");
+// });
+
+
+event.on("checkPage",(code,msg)=>{
+    console.log(`status code is ${code} and the page is ${msg}`);
 });
 
 
+event.emit("checkPage",200,"ok");
 
-event.emit("my name is");
+
+
+
+
+
+
+
+
+
+
+
