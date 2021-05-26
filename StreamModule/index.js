21720875ba92b34e8dce0fa6308cdb98c0b27f43
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-26 12:25:42 +0545

    reworked on streaming

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -1,19 +1,19 @@
 
-const fs = require('fs');
-const http = require('http');
+// const fs = require('fs');
+// const http = require('http');
 
 
-const server = http.createServer();
+// const server = http.createServer();
 
 
-server.on("request",(req,res)=>{
+// server.on("request",(req,res)=>{
 
-    // var fs = require('fs');
-    fs.readFile('input.txt',  (err,data)=>{
-        if (err) return console.error(err);
-        res.end(data.toString());
-    });
-});
+//     // var fs = require('fs');
+//     fs.readFile('input.txt',  (err,data)=>{
+//         if (err) return console.error(err);
+//         res.end(data.toString());
+//     });
+// });
 
 // this simply shows the data, not the actual streaming
 
@@ -22,21 +22,62 @@ server.on("request",(req,res)=>{
 // - create a readable stream
 // -    handle stream events ----> data, end, and errors
 
-const rsm = fs.createReadStream("input.txt");
+// const rsm = fs.createReadStream("input.txt");
 
-rsm.on('data',(chunkdata)=>{
+// rsm.on('data',(chunkdata)=>{
+//     res.write(chunkdata);
+// });
+
+// rsm.on("end",()=>{
+//     res.end();
+// });
+
+// rsm.on('error',(err)=>{
+//     console.log(err);
+//     res.end("file not found");
+// });
+
+// server.listen(8080,"127.0.0.1",()=>{
+//     console.log("server is created at port 8080");
+// });
+
+
+
+
+
+const { reset } = require('chalk');
+const fs = require('fs');
+const http = require('http');
+
+
+const server = http.createServer();
+
+server.on("request",(req,res)=>{
+    // fs.readFile("input.txt","utf-8",(err,data)=>{
+    //     if(err) return console.error(err);
+    //         res.end(data);
+    // });
+
+
+    const crstm = fs.createReadStream("input.txt");
+
+crstm.on('data',(chunkdata)=>{
     res.write(chunkdata);
+   
 });
 
-rsm.on("end",()=>{
+crstm.on('end',()=>{
     res.end();
 });
 
-rsm.on('error',(err)=>{
-    console.log(err);
+crstm.on('error',(err)=>{
     res.end("file not found");
 });
+    
+});
 
-server.listen(8080,"127.0.0.1",()=>{
-    console.log("server is created at port 8080");
+
+
+server.listen(8080,'127.0.0.1',()=>{
+    console.log("server has been created on port 8080");
 });
