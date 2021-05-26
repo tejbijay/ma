ea9855b7397d1bd36769332ac2c875234b6e5b14
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-26 11:37:59 +0545

    trying events again

--- EventMod/index.js
+++ EventMod/index.js
@@ -5,11 +5,11 @@
 // });
 // event.emit("my name is");
 
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 // const { emit } = require('process');
 
 
-const event = new EventEmitter();
+// const event = new EventEmitter();
 
 
 // event.on("my name is",()=>{
@@ -17,12 +17,37 @@ const event = new EventEmitter();
 // });
 
 
-event.on("checkPage",(code,msg)=>{
-    console.log(`status code is ${code} and the page is ${msg}`);
+// event.on("checkPage",(code,msg)=>{
+//     console.log(`status code is ${code} and the page is ${msg}`);
+// });
+
+
+// event.emit("checkPage",200,"ok");
+
+
+
+
+
+const EventEmitter = require('events');
+// const { emit } = require('process');
+
+const event = new EventEmitter();
+
+
+// event.on("my name is",()=>{
+//     console.log("tej");
+// });
+
+
+// event.emit("my name is");
+
+
+event.on("show",(code,msg,errors)=>{
+    console.log(`status code is ${code} and the page is ${msg} but other pages are ${errors}`);
 });
 
 
-event.emit("checkPage",200,"ok");
+event.emit("show",200,"ok",404);
 
 
 
