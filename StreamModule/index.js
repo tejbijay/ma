0faa236c7ff47a1dfb39859ba512bc82b2758de7
Tej Singh Chaudhary <tej.bijay@gmail.com>
2021-05-26 12:44:39 +0545

    reworked on streaming

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -45,38 +45,83 @@
 
 
 
-const { reset } = require('chalk');
+// const { reset } = require('chalk');
+// const fs = require('fs');
+// const http = require('http');
+
+
+// const server = http.createServer();
+
+// server.on("request",(req,res)=>{
+//     // fs.readFile("input.txt","utf-8",(err,data)=>{
+//     //     if(err) return console.error(err);
+//     //         res.end(data);
+//     // });
+
+
+//     const crstm = fs.createReadStream("input.txt");
+
+// crstm.on('data',(chunkdata)=>{
+//     res.write(chunkdata);
+   
+// });
+
+// crstm.on('end',()=>{
+//     res.end();
+// });
+
+// crstm.on('error',(err)=>{
+//     res.end("file not found");
+// });
+    
+// });
+
+
+
+// server.listen(8080,'127.0.0.1',()=>{
+//     console.log("server has been created on port 8080");
+// });
+
+
+
+
+
+
+
+const { error } = require('console');
 const fs = require('fs');
 const http = require('http');
 
 
 const server = http.createServer();
 
+
 server.on("request",(req,res)=>{
+
+
     // fs.readFile("input.txt","utf-8",(err,data)=>{
-    //     if(err) return console.error(err);
-    //         res.end(data);
+
+    //     if(err) return console.log(err);
+    //     res.end(data);
+
     // });
 
 
-    const crstm = fs.createReadStream("input.txt");
+    const rstream = fs.ReadStream("inputs.txt");
 
-crstm.on('data',(chunkdata)=>{
-    res.write(chunkdata);
-   
+    rstream.on("data",(chunkdata)=>{
+        res.write(chunkdata);
+    });
+
+    rstream.on("end",()=>{
+        res.end();
+    });
+
+    rstream.on("error",(err)=>{
+        res.end("file not found");
+    });
+
 });
-
-crstm.on('end',()=>{
-    res.end();
-});
-
-crstm.on('error',(err)=>{
-    res.end("file not found");
-});
-    
-});
-
-
 
 server.listen(8080,'127.0.0.1',()=>{
     console.log("server has been created on port 8080");
